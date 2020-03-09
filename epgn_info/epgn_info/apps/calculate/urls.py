2d3ef164789f8f125dcc7d5e1187fb8e03f574b7
PySuper <1821764535@qq.com>
2020-03-09 15:41:49 +0800

    优化URL导入

--- epgn_info/epgn_info/apps/calculate/urls.py
+++ epgn_info/epgn_info/apps/calculate/urls.py
@@ -1,32 +1,38 @@
-from . import views
+from .views import *
 from django.conf.urls import url
 
 urlpatterns = [
     # 返回文件通道信息
-    url(r'^channel/$', views.get_channel),
+    url(r'^channel/$', get_channel),
 
     # 返回文件列表 去重后的 通道列表和算法列表
-    url(r'^channel_list/$', views.get_channel_list),
+    url(r'^channel_list/$', get_channel_list),
 
     # 返回 后台处理 的算法结果
-    url(r'^calculate/$', views.calculate),
+    url(r'^calculate/$', calculate),
     # url(r'^calculate/$', views.ParseTask.as_view({"post": "parse_tasks"})),  # 使用类视图执行 多任务处理算法数据
 
     # 调用算法结果，直接返回数据
     # url(r'^get_file_result/$', views.get_file_result),
 
     # return_file_list
-    url(r'file_list/', views.return_file_list),
+    url(r'file_list/', return_file_list),
 
     # judge_file_channel
-    url(r'judge_file_channel/', views.judge_file_channel),
+    url(r'judge_file_channel/', judge_file_channel),
 
     # autocalculate
-    url(r'autocalculate/', views.auto_calculate),
+    url(r'autocalculate/', auto_calculate),
+
+    # 生成PPT
+    url(r'parse_ppt/', parse_ppt),
+
+    # 下载PPT
+    url(r'download_ppt/', download_ppt),
 
     # download
-    url(r'download_ppt/', views.download_ppt),
+    url(r'download_ppt/', download_ppt),
 
     # test_request
-    url(r'test_request/', views.test_request)
+    url(r'test_request/', test_request)
 ]

fad6b5c34c819acddcb42538d4d110a9a3cbe999
PySuper <1821764535@qq.com>
2020-03-19 08:39:50 +0800

    去除参考通道

--- epgn_info/scripts/process_gecent.py
+++ epgn_info/scripts/process_gecent.py
@@ -1,16 +1,16 @@
 import os
 import re
 from multiprocessing import cpu_count, Pool, Manager
-from scripts.read_hdf import read_hdf   # manage
+from scripts.read_hdf import read_hdf  # manage
 from epgn_info.apps.calculate.algorithm.class_calculate_two import *  # manage
-from epgn_info.apps.calculate.algorithm.calculate_name import CalculateNameDict   # manage
+from epgn_info.apps.calculate.algorithm.calculate_name import CalculateNameDict  # manage
 # from epgn_info.scripts.read_hdf import read_hdf   # Nginx
 # from epgn_info.epgn_info.apps.calculate.algorithm.class_calculate_two import *  # Nginx
 # from epgn_info.epgn_info.apps.calculate.algorithm.calculate_name import CalculateNameDict   # Nginx
-from settings.devp import ChannelList
+from epgn_info.settings.devp import CHANNEL_LIST
 
 
-class ParseTask():
+class ParseTask(object):
     """
     Note：这部分有两个部分（一个一个实现）
         1. IO密集：读取文件信息（多线程），把所有的IO写在一个函数里
@@ -51,17 +51,23 @@ class ParseTask():
                 # data_key
 
                 # TODO： 在这里修改当前获取到的ChannelName
-                if channel["title"] in ChannelList:
-                    channelname = channel["title"]
-                else:
-                    raise Exception
-                channel_data_key = list(channel_file_list.keys())[list(channel_file_list.values()).index(channelname)]
+                # if channel["title"] in ChannelList:
+                #     channelname = channel["title"]
+                # else:
+                #     raise Exception
+                # channel_data_key = list(channel_file_list.keys())[list(channel_file_list.values()).index(channelname)]
 
+                print(channel["title"], channel_file_list)
                 # channel_data_key = list(channel_file_list.keys())[list(channel_file_list.values()).index(channel["title"])]
+                channel_data_key = list(channel_file_list.keys())[list(channel_file_list.values()).index(channel["title"])]
                 channel_data_num = re.match(r'.*?(\d+)', channel_data_key).group(1)
-                # rpm_key
-                channel_rpm = list(channel_file_list.keys())[list(channel_file_list.values()).index("EngineRPM")]
-                channel_rpm_num = re.match(r'.*?(\d+)', channel_rpm).group(1)
+                try:
+                    # rpm_key
+                    channel_rpm = list(channel_file_list.keys())[list(channel_file_list.values()).index("EngineRPM")]
+                    # channel_rpm = list(channel_file_list.keys())[list(channel_file_list.values()).index("VehicleSpeed")]
+                    channel_rpm_num = re.match(r'.*?(\d+)', channel_rpm).group(1)
+                except:
+                    channel_rpm_num = 2
 
                 """异步返回"""
                 # 使用队列接受多进程中的数据返回值
@@ -80,7 +86,8 @@ class ParseTask():
         pool.close()
         pool.join()
 
-    def calculate_process(self, queue, calculate_class_name, file_name, channel_data, channel_name, raw_time_num, raw_data_num, raw_rpm_num):
+    def calculate_process(self, queue, calculate_class_name, file_name, channel_data, channel_name, raw_time_num,
+                          raw_data_num, raw_rpm_num):
         """
         返回图片
         # img_path = eval(calculate_class_name)(file_name, channel_data, channel_name, raw_time_num, raw_data_num, raw_rpm_num).run()
@@ -88,7 +95,8 @@ class ParseTask():
         """
         # 返回item
         try:
-            print(calculate_class_name, os.getpid())
+            print(file_name, os.getpid(), channel_name, raw_data_num)
+            print(calculate_class_name, channel_name, raw_time_num, raw_data_num, raw_rpm_num)
             X, Y = eval(calculate_class_name)(file_name, channel_data, channel_name, raw_time_num, raw_data_num, raw_rpm_num).run()
             # 这里确定返回到前端的数据
             queue.put({"filename": file_name, "data": {"X": X, "Y": Y}})
@@ -99,18 +107,22 @@ class ParseTask():
         # 使用进程间通信 返回多个数据的返回
         # 返回items里面是当前数据计算的结果，而不是当前数据计算结果的图片
         items = []
-        # self.parse_file_info()
-        # while True:
-        #     if self.queue.empty():
-        #         break
-        #     items.append(self.queue.get())
-        # return items
-        try:
-            self.parse_file_info()
-            while True:
-                if self.queue.empty():
-                    break
-                items.append(self.queue.get())
-            return items
-        except Exception as e:
-            return items
+        self.parse_file_info()
+        while True:
+            if self.queue.empty():
+                break
+            items.append(self.queue.get())
+        return items
+        # try:
+        #     self.parse_file_info()
+        #     while True:
+        #         if self.queue.empty():
+        #             break
+        #         items.append(self.queue.get())
+        #     return items
+        # except Exception as e:
+        #     return items
+
+
+class AutoParseTask(ParseTask):
+    pass

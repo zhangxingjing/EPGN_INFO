daf6c0a573d8a0bf45e3a6e5b5b87c3c5451ab49
PySuper <1821764535@qq.com>
2020-03-19 08:42:08 +0800

    更新项目全局变量

--- epgn_info/epgn_info/settings/devp.py
+++ epgn_info/epgn_info/settings/devp.py
@@ -81,8 +81,8 @@ DATABASES = {
         'PORT': 3306,  # 数据库端口
         'USER': 'root',  # 数据库用户名
         'PASSWORD': 'root',  # 数据库用户密码
-        'NAME': 'epgn',  # 新建数据库
-        # 'NAME': 'EPGN_INFO',  # 新建数据库==> 使用xadmin
+        # 'NAME': 'epgn',  # 新建数据库
+        'NAME': 'EPGN_INFO',  # 新建数据库==> 使用xadmin
         'OPTIONS': {
             'read_default_file': os.path.dirname(os.path.abspath(__file__)) + '/my.cnf',
             'init_command': 'SET sql_mode=STRICT_TRANS_TABLES',
@@ -271,5 +271,27 @@ XADMIN_TITLE = "EPGN_INFO 后台管理"  # 左上方的文字
 XADMIN_FOOTER_TITLE = "small.spider.p@gmail.com"  # 最下面的文字
 
 # 配置全局`文件`路径
-ChannelList = ["VR", "VL", "HR", "HL"]
-FileSavePath = "/home/zheng/Desktop/demo/R_HDF/"
+CHANNEL_LIST = ["VR", "VL", "HR", "HL", "vorn rechits", "vorn links", "hinten rechits", "hinten links"]
+FILE_SAVE_PATH = "/home/zheng/Desktop/demo/R_HDF/"
+CALCULATE_RULE = {
+    "(N)G3 VZ": "Level VS RPM",
+    "(N)G3 VS": "Level VS RPM",
+    "(N)G5 VZ": "2nd Order VS RPM",
+    "KP 80-20": "FFT",
+    "(Square&Lab)Leerlauf D Gang mit AC": "Level VS Time",
+    "(Square&Lab)Leerlauf P Gang mit AC": "Level VS Time",
+    "(Square&Lab)Leerlauf R Gang mit AC": "Level VS Time",
+    "(Square&Lab)Leerlauf N Gang mit AC": "Level VS Time",
+
+    "(Square&Lab)Leerlauf D Gang ohne Verbrauche": "Level VS Time",
+    "(Square&Lab)Leerlauf P Gang ohne Verbrauche": "Level VS Time",
+    "(Square&Lab)Leerlauf R Gang ohne Verbrauche": "Level VS Time",
+    "(Square&Lab)Leerlauf N Gang ohne Verbrauche": "Level VS Time",
+
+    "(Square&Lab)Leerlauf D Gang mit Verbrauche": "Level VS Time",
+    "(Square&Lab)Leerlauf P Gang mit Verbrauche": "Level VS Time",
+    "(Square&Lab)Leerlauf R Gang mit Verbrauche": "Level VS Time",
+    "(Square&Lab)Leerlauf N Gang mit Verbrauche": "Level VS Time",
+    "Start-Stop": "滤波器，目前还没有!"
+}
+REFERENCE_CHANNEL = ["time", "EngineRPM", "EngineCoolantTemp", "VehicleSpeed"]

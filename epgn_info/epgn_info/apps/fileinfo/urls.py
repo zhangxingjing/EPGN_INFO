7867a107108db7c9094fae177ed35f50a595f6a9
PySuper <1821764535@qq.com>
2020-04-13 09:45:20 +0800

    更新文件模块的url

--- epgn_info/epgn_info/apps/fileinfo/urls.py
+++ epgn_info/epgn_info/apps/fileinfo/urls.py
@@ -1,65 +1,68 @@
-from . import views
+from .views import *
 from django.conf.urls import url
 from rest_framework.routers import SimpleRouter, DefaultRouter
 
 router = SimpleRouter()
-router.register(r'parse_file', views.FileInfoViewSet)
+router.register(r'parse_file', FileInfoViewSet)
 
 urlpatterns = [
     # 检索 ==> 表格重载
-    url(r'^car/$', views.file),
+    url(r'^car/$', file),
 
-    # 文件上传
-    url(r'^upload/$', views.upload),
+    # 文件上传及通道修改
+    # url(r'^upload/$', upload),
+    url(r'^upload/$', ParseFile.as_view()),
 
     # 文件下载
-    url(r'^download/(?P<pk>\d+)$', views.file_down),
-
-    # 获取平台
-    url(r'^car_platform/$', views.PlatformCarModelView.as_view({'get': 'platform'})),
-
-    # 获取所有车型
-    url(r'^car_model/$', views.PlatformCarModelView.as_view({'get': 'every_platform'})),
-
-    # 获取车型
-    url(r'^car_model/(?P<pk>\d+)$', views.PlatformCarModelView.as_view({'get': 'car_model'})),
-
-    # 获取动力总成
-    url(r'^propulsionpower_num/$', views.PropulsionPowerView.as_view({'get': 'propulsion'})),
-
-    # 获取功率
-    url(r'^propulsionpower_num/(?P<pk>\d+)/$', views.PropulsionPowerView.as_view({'get': 'power'})),
-
-    # 获取所有功率
-    url(r'^power/$', views.PropulsionPowerView.as_view({'get': 'every_power'})),
-
-    # 获取专业方向
-    url(r'^direction_num/$', views.DirectionView.as_view({'get': 'parts'})),
-
-    # 获取零部件和工况
-    url(r'^direction_num/(?P<pk>\d+)/$', views.DirectionView.as_view({'get': 'power'})),
-
-    # 获取变速箱信息
-    url(r'gearbox/$', views.GearBoxView.as_view({'get': 'get_gearbox'})),
-
-    # 用户添加对比列表
-    # url(r'^contrast/$', views.SaveContrastView.as_view()),
+    url(r'^download/(?P<pk>\d+)$', file_down),
 
     # 用户查看使用文档
-    url(r'^word/$', views.word),
+    url(r'^word/$', word),
+
+    # check文件通道信息
+    url(r'channel_check/$', CheckChannel.as_view()),
 
     # 访问模板渲染页面
-    url(r'^base/(?P<pk>\d+)/$', views.parse_template),
+    url(r'^base/(?P<pk>\d+)/$', parse_template),
+
+    # 用户添加对比列表
+    # url(r'^contrast/$', SaveContrastView.as_view()),
 
     # 用户撤销文件上传
-    # url(r'^cancel/$', views.cancel),
+    # url(r'^cancel/$', cancel),
 
     # 获取"当前用户"上传的数据信息
-    # url(r'user_file_info/$', views.user_file_info),
+    # url(r'user_file_info/$', user_file_info),
 
+    # 获取平台
+    url(r'^car_platform/$', PlatformCarModelView.as_view({'get': 'platform'})),
+
+    # 获取所有车型
+    url(r'^car_model/$', PlatformCarModelView.as_view({'get': 'every_platform'})),
+
+    # 获取车型
+    url(r'^car_model/(?P<pk>\d+)$', PlatformCarModelView.as_view({'get': 'car_model'})),
+
+    # 获取动力总成
+    url(r'^propulsionpower_num/$', PropulsionPowerView.as_view({'get': 'propulsion'})),
+
+    # 获取功率
+    url(r'^propulsionpower_num/(?P<pk>\d+)/$', PropulsionPowerView.as_view({'get': 'power'})),
+
+    # 获取所有功率
+    url(r'^power/$', PropulsionPowerView.as_view({'get': 'every_power'})),
+
+    # 获取专业方向
+    url(r'^direction_num/$', DirectionView.as_view({'get': 'parts'})),
+
+    # 获取零部件和工况
+    url(r'^direction_num/(?P<pk>\d+)/$', DirectionView.as_view({'get': 'power'})),
+
+    # 获取变速箱信息
+    url(r'gearbox/$', GearBoxView.as_view({'get': 'get_gearbox'})),
 ]
 
 # 搜索引擎的URL
-# router.register('fileinfo/search', views.FileSearchViewSet, base_name='fileinfo_search')
+# router.register('fileinfo/search', FileSearchViewSet, base_name='fileinfo_search')
 
 urlpatterns += router.urls

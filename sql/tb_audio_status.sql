04a90dfdc3bf21b194f20e4210d886b07c516f87
small-spider <1821764535@qq.com>
2020-09-03 15:24:06 +0800

    修改-线上的调整

--- sql/tb_audio_status.sql
+++ sql/tb_audio_status.sql
@@ -4,3 +4,4 @@ insert into `tb_audio_status` values ('3', '滚动噪声/轮胎噪声');
 insert into `tb_audio_status` values ('4', '怠速');
 insert into `tb_audio_status` values ('5', '启停');
 insert into `tb_audio_status` values ('6', '底盘舒适性');
+insert into `tb_audio_status` values ('7', '静态(附件噪声)');
